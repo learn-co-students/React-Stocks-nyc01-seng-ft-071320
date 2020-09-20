d3ecff8e6a6cff3303334847c794594490689096
humorous-storage-7521 <rmwerner@umich.edu>
2020-09-19 19:29:40 -0500

    do first 2 deliverables

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,25 +5,60 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    loaded: false,
+    stocks: [],
+    portfolio: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/stocks')
+    .then(resp => resp.json())
+    .then(stocks => {
+      console.log(stocks)
+      this.setState({
+        loaded: true,
+        stocks: stocks
+      })
+    })
+  } 
+
+  stockClickListener = (stock) => {
+    let newArray = this.state.portfolio
+    if (!newArray.find(portStock => portStock.id === stock.id)) {
+      newArray.push(stock)
+      this.setState({portfolio: newArray})
+    }
+  }
+
+  portfolioClickListener = (stock) => {
+    let newArray = this.state.portfolio
+    let index = newArray.findIndex(portStock => portStock.id === stock.id)
+    newArray.splice(index, 1)
+    this.setState({portfolio: newArray})
+  }
+
   render() {
-    return (
+    return this.state.loaded
+      ?
       <div>
         <SearchBar/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.stocks} clickListener={this.stockClickListener} />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={this.state.portfolio} clickListener={this.portfolioClickListener} />
 
             </div>
           </div>
       </div>
-    );
+      :
+      <h1>loading...</h1>
   }
 
 }
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -3,12 +3,16 @@ import Stock from '../components/Stock'
 
 class StockContainer extends Component {
 
+  stocks = () => {
+    return this.props.stocks.map(stock => <Stock key={stock.id} stock={stock} clickListener={this.props.clickListener} />)
+  }
+
   render() {
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          this.stocks()
         }
       </div>
     );
